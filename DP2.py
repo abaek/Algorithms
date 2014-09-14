937d7f36ec9e7938e84fc81147efac73b94a7637
Andy Baek <andybaek95@gmail.com>
2014-09-13 23:21:02 -0400

    More DP

--- DP2.py
+++ DP2.py
@@ -1,6 +1,8 @@
 #source: http://codercareer.blogspot.ca/p/dynamic-interview-questions.html
 
-#Maximum Sum of All Consecutive Sub-arrays
+"""
+Maximum Sum of All Consecutive Sub-arrays
+"""
 def maxSumSubArray(ar):
 	lengthAr = len(ar)
 	maxSumEndIndex = [-1 for i in range(lengthAr)]
@@ -13,8 +15,9 @@ def maxSumSubArray(ar):
 
 #print maxSumSubArray([1, -2, 3, 10, -4, 7, 2, -5])
 
-
-#Maximal Length of Incremental Subsequences
+"""
+Maximal Length of Incremental Subsequences
+"""
 def maxIncreasingSubSequence(ar):
 	lengthAr = len(ar)
 	maxEndAtIndex = [-1 for i in range(lengthAr)]
@@ -32,8 +35,31 @@ def maxIncreasingSubSequence(ar):
 #Implement a function which gets the edit distance of two input strings.
 #There are three types of edit operations: insertion, deletion and substitution.
 #Edit distance is the minimal number of edit operations to modify a string from one to the other.
+def edit(str1, str2):
+	str1len = len(str1)
+	str2len = len(str2)
 
+	#result[i][j] = min edit distance from first i characters to first j characters
+	result = [[-1 for j in range(str2len+1)] for i in range(str1len+1)]
+	
+	#set first row and column
+	for i in range(str1len+1):
+		result[i][0] = i
+	for i in range(str2len + 1):
+		result[0][i] = i
 
+	#set rest of table
+	for i in range(1, str1len + 1):
+		for j in range(1, str2len+1):
+			if str1[i-1] == str2[j-1]:
+				result[i][j] = result[i-1][j-1]
+			else:
+				result[i][j] = min(result[i][j-1], result[i-1][j], result[i-1][j-1]) + 1
+
+	for line in result: print line
+	return result[str1len][str2len]
+
+print edit("sunday", "saturday")
 
 
 def coinProblem(ar, val):
@@ -94,5 +120,51 @@ def backpack(items, weight):
 				result[i][curWeight] = result[i-1][curWeight]
 	for line in result: print line
 	return result[len(items)-1][weight]
+
+#print backpack([(10, 8), (7, 8), (3, 3)], 10)
+
+def backpack2(items, weight):
+	#result[i] = max value with max up to weight i
+	result = [-1 for i in range(weight+1)]
+	result[0] = 0
+	for curWeight in range(1, weight+1):
+		result[curWeight] = 0
+		for item in items:
+			if item[0] <= curWeight:
+				result[curWeight] = max(result[curWeight], result[curWeight - item[0]] + item[1])
+	print result
+	return result[weight]
+
+#print backpack2([(10, 8), (7, 8), (3, 3)], 10)
+
+
+
+
+
+
+#theif
+def theif(houses):
+	#result[i] = max value from first i houses
+	result = [-1 for i in range(len(houses))]
+	result[0] = houses[0]
+	result[1] = max(houses[0], houses[1])
+	for i in range(2, len(houses)):
+		result[i] = max(houses[i] + result[i-2], reuslt[i-1])
+	print result
+	return result[len(houses)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
