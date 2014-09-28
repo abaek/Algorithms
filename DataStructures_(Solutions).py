91c1eb9ebdae660910f63c5dc6c3ad3205030466
Andy Baek <andybaek95@gmail.com>
2014-09-28 02:40:37 -0400

    heap

--- DataStructures_(Solutions).py
+++ DataStructures_(Solutions).py
@@ -84,6 +84,125 @@ def sortStack(stack):
 	while(True):
 		pass
 
+"""
+6) Implement a Heap
+"""
+#implement a min heap (using an array)
+class Heap: #minHeap
+	def __init__(self, size=1):
+		self.size = size
+		self.ar = [-1 for i in range(size)]
+
+	#build up
+	def createFromArray(self, ar):
+		self.size = len(ar)
+		#loop through indexes backwards starting from midpoint
+		for i in range(len(ar)/2, -1 , -1):
+			index = i
+			#coninually swap down left side of heap until property statisfied
+			while(index < self.size):
+				print ar, index, 'l'
+				left = index*2 + 1
+				right = index*2 + 2
+				if left < self.size and ar[left] < ar[index]:
+					#swap
+					temp = ar[index]
+					ar[index] = ar[left]
+					ar[left] = temp
+					#for every swap left, make sure right still holds
+					indexRight = index
+					while(indexRight < self.size):
+						print ar, index, 'r - in'
+						right = indexRight*2 + 2
+						if right < self.size and ar[right] < ar[indexRight]:
+							#swap
+							temp = ar[indexRight]
+							ar[indexRight] = ar[right]
+							ar[right] = temp
+							indexRight = right
+						else:
+							break
+					index = left
+				elif right < self.size and ar[right] < ar[index]:
+					print ar, index, 'r - out'
+					#swap
+					temp = ar[index]
+					ar[index] = ar[right]
+					ar[right] = temp
+					index = right
+				else:
+					break
+		self.ar = ar
+
+	def push(self, val):
+		#insert into first instance of -1
+		newPos = -1
+		for i in range(self.size):
+			if self.ar[i] == -1:
+				newPos = i
+				break
+		#double length of heap
+		if newPos == -1:
+			newPos = self.size
+			self.ar.extend([-1 for i in range(self.size)])
+			self.size = self.size * 2
+		#insert
+		self.ar[newPos] = val
+		#swap backwards until fit
+		curPos = newPos
+		while(curPos > 0):
+			if curPos%2 == 0:
+				parent = (curPos-2)/2
+			else:
+				parent = (curPos-1)/2
+			if self.ar[curPos] > self.ar[parent]:
+				break
+			#swap
+			self.ar[curPos] = self.ar[parent]
+			self.ar[parent] = val
+			curPos = parent
+			
+
+	#Always pop root
+	def pop(self):
+		index = 0
+		#pop root and swap down heap
+		while index*2+1 < self.size:
+			#right is too big
+			if index*2+2 >= self.size:
+				#left DNE
+				if self.ar[index*2+1] == -1:
+					break
+				#swap left
+				self.ar[index] = self.ar[index*2 + 1]
+				index = index*2+1
+			else:
+				#left does not exist
+				if self.ar[index*2+1] == -1:
+					#right DNE
+					if self.ar[index*2+2] == -1:
+						break
+					#swap right
+					self.ar[index] = self.ar[index*2 + 2]
+					index=index*2+2
+				#right DNE, swap left
+				elif self.ar[index*2+2] == -1:
+					self.ar[index] = self.ar[index*2 + 1]
+					index=index*2+1
+				#both exist
+				else:
+					if self.ar[index*2 + 1] < self.ar[index*2 + 2]:
+						self.ar[index] = self.ar[index*2 + 1]
+						index = index*2 + 1
+					else:
+						self.ar[index] = self.ar[index*2 + 2]
+						index = index*2 + 2
+		self.ar[index] = -1
+
+#tests:
+# h = Heap()
+# h.createFromArray([18, 5, 6, 20, 3, 12, 10, 13, 4])
+
 
 
 
