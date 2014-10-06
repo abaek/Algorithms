cac0a42e1079288c1b6fa8ab61b764b55d46f20d
Andy Baek <andybaek95@gmail.com>
2014-10-06 15:27:01 -0400

    bit addition

--- BitManipulation_(Solutions).py
+++ BitManipulation_(Solutions).py
@@ -7,12 +7,27 @@
 #-------------------------------------------------------------------------------
 
 """
-1) Do binary addition
+1) Do binary addition (with only positive integers)
 """
-def binAdd(num1, num2):
-	return bin(int(num1, 2) + int(num2, 2))
+def bitAdd(num1, num2):
+	carry = 0
+	result = 0
+	for i in range(32):
+		curPos = 2**i
+		#sum = 0, 1, 2, or 3
+		sum = ((curPos & num1) >> i) + ((curPos & num2) >> i) + carry
+		if sum >= 2:
+			carry = 1
+		else:
+			carry = 0
+		if sum % 2 == 1:
+			result += curPos
+	return result
 #tests:
-assert binAdd('010', '1011') == bin(13)
+assert bitAdd(1, 1) == 2
+assert bitAdd(1, 13) == 14
+assert bitAdd(34, 324) == 358
+assert bitAdd(23, 0) == 23
 
 
 """
